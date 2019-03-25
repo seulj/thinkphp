0965a4cf232d4090591e600de07e0c1ad7a2f1e2
seulj <lijing@corp-ci.com>
2019-03-25 17:50:57 +0800

    no message

--- application/api/controller/Content.php
+++ application/api/controller/Content.php
@@ -11,5 +11,17 @@ namespace app\api\controller;
 
 class Content
 {
+    public function getList()
+    {
+        $page = input('page') ?: 1;
+        $page = intval($page);
+        $limit = config('paginate')['list_rows'];
+        $data['list'] = model('content')->getByPage($page, $limit);
+        $data['page'] = model('content')->getPage();
+        if ($data['list']) {
+            return json(['ret' => 1, 'data' => $data]);
+        } else
+            return json(['ret' => 0, 'data' => 'end']);
+    }
 
 }
--- application/api/model/Content.php
+++ application/api/model/Content.php
@@ -11,5 +11,22 @@ namespace app\api\model;
 
 class Content
 {
+    public function getByPage($page, $limit)
+    {
+        $query = db('content')->page($page, $limit)->select();
+        return $query;
+    }
+
+    public function getPage()
+    {
+        $query = db('content')->count();
+        $page = ceil($query / config('paginate')['list_rows']);
+        return $page;
+    }
+
+    public function getContent($id)
+    {
+        return db('content')->where('id', $id)->find();
+    }
 
 }
