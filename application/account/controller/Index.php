e0ef174c39f1e7268d5900a4986f6226e39033e0
seulj <lijing@corp-ci.com>
2019-03-28 15:08:30 +0800

    no message

--- application/account/controller/Index.php
+++ application/account/controller/Index.php
@@ -8,10 +8,11 @@ class Index
 {
     public function index()
     {
-        $params['phone'] = '17775210320';
-        $json = myRequest('https://www.mental.com/index.php/api/User/verifyPhone', 'POST', $params);
-        $data = json_decode($json, true);
-        return $json(['data' => $data]);
+//        $params['phone'] = '17775210320';
+//        $json = myRequest('https://www.mental.com/index.php/api/User/verifyPhone', 'POST', $params);
+//        $data = json_decode($json, true);
+        $data = 111;
+        return json(['data' => $data]);
     }
 
     public function getCaptcha()
